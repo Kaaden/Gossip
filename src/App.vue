c50d2a389aa424f66c5db53e4a4a60f8fc702d04
kaaden <794102675@qq.com>
2019-06-03 11:51:30 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<style>
+   * {
+        font-size: 0.16rem;
+    }
+</style>
+
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data() {
+      return {
+        routName: ''
+      }
+    },
+    mounted() {
+      this.routName = this.$route.name
+    },
+    updated() {
+      this.routName = this.$route.name
+    }
+  }
+ 
+</script>
+
+
+
